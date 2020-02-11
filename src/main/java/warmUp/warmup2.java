b2fa728c81c2e115c2f37a6983cac12dcd1dead2
Aflatun Velibeyli <a.velibeyli6@gmail.com>
2020-02-11 11:10:48 +0400

    adding everything

--- src/main/java/warmUp/warmup2.java
+++ src/main/java/warmUp/warmup2.java
@@ -1,23 +1,33 @@
 package warmUp;
 import java.util.Scanner;
+import java.util.*;
 public class warmup2 {
+    static public boolean isShoudBeFilled(int i,int j,int row,int column,double tan){
+        return (i==1 || i==(row) || j==1 || j==(column) || (j==(int)(i*tan)) ||(column-j==(int)(i*tan)) );
+    }
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        int row,column;
+        final int row,column;
         System.out.print("Enter row: ");
         row=in.nextInt();
         System.out.print("Enter column: ");
         column=in.nextInt();
-        for(int i=0;i<row;i++){
-            for(int j=0;j<column;j++){
-                if(i==0||i==(row-1)){
-                    System.out.print("*");
-                }if(j==0||j==(column-1)){
-                    System.out.print("*");
+        StringBuilder envelope=new StringBuilder(); // IS TO REPLACE SOUT WITH SPEED CLASS!!
+        double tan = ((double)column)/row;  //Take a good look at that
+        for(int i=1;i<=row;i++){
+            for(int j=1;j<=column;j++){
+                if(isShoudBeFilled(i,j,row,column,tan)){
+//                    System.out.print("*");
+                    envelope.append("*");
                 }
-            }
-            System.out.println();
-        }
+                else{
+//                    System.out.print(" ");
+                    envelope.append(" ");
+                }
 
+            }
+            envelope.append("\n");
+        }
+        System.out.println(envelope);
     }
 }
