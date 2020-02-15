34ca8d8aca703e632573805765cf37dab7192633
Yagut-YYAG <yagut.badalova@gmail.com>
2020-02-15 10:21:31 +0400

    created 2 functions for filling & combining arrays

--- src/main/java/warmUp/saturday15.java
+++ src/main/java/warmUp/saturday15.java
@@ -1,15 +1,16 @@
 package warmUp;
 import java.util.Random;
 public class saturday15 {
-    public static void main(String[] args) {
-        Random random=new Random();
-        int[] array1=new int[5];
-        int[] array2=new int[5];
-        int[] array3=new int[10];
-        for(int i=0;i<5;i++){
-            array1[i]=(int)(Math.random()*100);
-            array2[i]=(int)(Math.random()*100);
-        }int a=0;
+
+    public static int[] fill(int[] array){
+        for(int i=0;i<array.length;i++){
+            array[i]=(int)(Math.random()*100);
+        }
+        return array;
+    }
+    public static int[] combine(int[] array1,int[] array2){
+        int[] array3=new int[array1.length+array2.length];
+        int a=0;
         for(int i:array1){
             array3[a]=i;
             a+=2;
@@ -19,16 +20,31 @@ public class saturday15 {
             array3[b]=i;
             b+=2;
         }
-        for(int i=0;i<5;i++){
+        return array3;
+    }
+
+    public static void main(String[] args) {
+        Random random=new Random();
+        final int length=5;
+        int[] array1=new int[length];
+        int[] array2=new int[length];
+        int[] array3=new int[length*2];
+        array1=fill(array1);
+        array2=fill(array2);
+        array3=combine(array1,array2);
+
+        // PRINTING OUT ///////////////////
+        for(int i=0;i<length;i++){
             System.out.print(array1[i]+"  ");
         }
         System.out.println();
-        for(int i=0;i<5;i++){
+        for(int i=0;i<length;i++){
             System.out.print(array2[i]+"  ");
         }
         System.out.println();
         for(int i:array3){
             System.out.print(i+" ");
         }
+        ///////////////////////////////
     }
 }
