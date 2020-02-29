5a775e7ca389b82eb6152fca12fc385ddf12b842
Yeskela7 <alekseyh215@gmail.com>
2020-02-29 10:42:01 +0400

    10:41 adding warm UP sentences

---
+++ src/main/java/warmUp/saturday29/MakeItBro.java
@@ -0,0 +1,24 @@
+package warmUp.saturday29;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class MakeItBro {
+    private static List<String> list;
+
+    public static ArrayList<String> make() {
+        list = new ArrayList<>();
+        return (ArrayList<String>) list;
+    }
+
+    public ArrayList<String> build(List<String> subjects, List<String> verbs, List<String> objects) {
+        StringBuilder print = new StringBuilder();
+        ArrayList<String> a =list.stream().map((s, v, o) -> (s + " " + v + " " + o + "\n")).collect(Collectors.toList());
+
+
+        return (ArrayList<String>) list;
+
+    }
+}
