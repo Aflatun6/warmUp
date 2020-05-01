df78fc8fa8ad3f6f468da8c4c5b7c3499cda4b10
Aflatun Velibeyli <a.velibeyli6@gmail.com>
2020-05-01 17:47:00 +0400

    recursion is done on JumpingONClouds

--- src/main/java/warmUp/friday1may/Clouds.java
+++ src/main/java/warmUp/friday1may/Clouds.java
@@ -1,35 +1,37 @@
 package warmUp.friday1may;
 
 public class Clouds {
-    static int jumpingOnClouds(int[] c) {
+    static int jumpingOnClouds2(int[] c) {
         int count = 0;
         int curr = 0;
         while (curr < c.length - 1) {
             if (curr + 2 < c.length && c[curr + 2] == 0) {
-
                 curr += 2;
                 count++;
-
             } else if (curr + 1 < c.length && c[curr + 1] == 0) {
-
                 curr++;
                 count++;
-
             }
-
         }
         return count;
     }
 
-    static int jumpintOnClouds2(int[] c) {
-        return recur(c, 0, 0);
+    public static void main(String[] args) {
+        System.out.println(jumpingOnClouds(new int[]{0, 0, 1, 0, 0, 1, 0}));
     }
 
-    static int recur(int[] c, int curr, int count) {
+    static int jumpingOnClouds(int[] c) {
+        return recuring(c, 0, 0);
+    }
+
+    private static int recuring(int[] c, int curr, int count) {
         if (curr == c.length - 1) return count;
         if (curr + 2 < c.length && c[curr + 2] == 0) {
-            return recur(c, curr + 2, count++);
+            return recuring(c, curr + 2, count + 1);
         }
-        return recur(c, curr + 1, count++);
+        else if (curr + 1 < c.length && c[curr + 1] == 0) {
+            return recuring(c, curr + 1, count + 1);
+        }
+        return 0;
     }
 }
