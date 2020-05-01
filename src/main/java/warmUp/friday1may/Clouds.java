b414f8e8c24faa79ec35bd3f31c94eb3fe617a55
Aflatun Velibeyli <a.velibeyli6@gmail.com>
2020-05-01 10:03:55 +0400

    unfinished warmUps on 1'st may

---
+++ src/main/java/warmUp/friday1may/Clouds.java
@@ -0,0 +1,35 @@
+package warmUp.friday1may;
+
+public class Clouds {
+    static int jumpingOnClouds(int[] c) {
+        int count = 0;
+        int curr = 0;
+        while (curr < c.length - 2) {
+            if (curr + 2 < c.length) {
+                if (c[curr + 2] == 0) {
+                    curr += 2;
+                    count++;
+                }
+            } else if (curr + 1 < c.length) {
+                if (c[curr + 1] == 0) {
+                    curr++;
+                    count++;
+                }
+            }
+
+        }
+        return count;
+    }
+
+    static int jumpintOnClouds2(int[] c) {
+        return recur(c, 0, 0);
+    }
+
+    static int recur(int[] c, int curr, int count) {
+        if (curr == c.length - 1) return count;
+        if (curr + 2 < c.length && c[curr + 2] == 0) {
+            return recur(c, curr + 2, count++);
+        }
+        return recur(c, curr + 1, count++);
+    }
+}
